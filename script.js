4b380e98c8ab19626e854b411de2c0bae3957779
Himanshuu03 <himanshujangra2403@gmail.com>
2023-07-07 23:06:14 +0530

    scripting

---
+++ script.js
@@ -0,0 +1,42 @@
+// let api = `https://api.github.com/users/${gitusername}`
+
+var getInputData = document.querySelector('[data-user-name]');
+var getInputDataBtn = document.querySelector('[data-search-user]');
+
+var userName = "";
+
+getInputDataBtn.addEventListener('click',()=>{
+    userName = getInputData.value;
+    fetchDevData();
+})
+
+async function fetchDevData(){
+    const api = await fetch(`https://api.github.com/users/${userName}`);
+    const result = await api.json();
+    renderData(result);
+}
+
+function renderData(result){
+    const userDataName = document.querySelector('[data-name]');
+    const userImg = document.querySelector('[data-img]');
+    const userDate = document.querySelector('[data-date]');
+    const userLink = document.querySelector('[data-link]');
+    const userDes = document.querySelector('[data-des]');
+    const userRepo = document.querySelector('[data-repo]');
+    const userFollower = document.querySelector('[data-follower]');
+    const userFollowing = document.querySelector('[data-following]');
+    const userLocation = document.querySelector('[data-user-location]');
+
+
+    
+    userImg.src = `${result.avatar_url}`
+    userDataName.innerText = `${result?.name}`;
+    userDate.innerText = `${result?.created_at.split('T')?.[0]}`
+    userLink.innerText =`${result?.login}`;
+    userLink.href =`${result?.html_url}`;
+    userDes.innerText =`${result?.bio}`
+    userRepo.innerText =`${result?.public_repos}`
+    userFollower.innerText =`${result?.followers}`
+    userFollowing.innerText=`${result?.following}`
+    userLocation.innerText =`${result?.location}`
+}
